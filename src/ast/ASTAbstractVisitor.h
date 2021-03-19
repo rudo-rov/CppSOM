8d571b2346609ddfcfc5923c9c27090e3254b134
Rudolf Rovnak <r.rovnak@gmail.com>
2021-03-19 10:10:25 +0100

    AST abstract visitor

--- src/ast/ASTAbstractVisitor.h
+++ src/ast/ASTAbstractVisitor.h
@@ -1,12 +1,64 @@
 #pragma once
-#include <any>
-#include "ASTNodes.h"
 
 namespace som {
 
+	// Forward declaration of AST nodes to resolve circular reference
+	struct Class;
+	struct Method;
+	struct Block;
+	struct NestedBlock;
+	struct UnaryPattern;
+	struct BinaryPattern;
+	struct KeywordPattern;
+	struct Keyword;
+	struct UnarySelector;
+	struct BinarySelector;
+	struct BinaryOperand;
+	struct KeywordSelector;
+	struct UnaryMessage;
+	struct BinaryMessage;
+	struct KeywordMessage;
+	struct Formula;
+	struct LiteralInteger;
+	struct LiteralDouble;
+	struct LiteralArray;
+	struct LiteralString;
+	struct Assignation;
+	struct Evaluation;
+	struct Variable;
+	struct NestedTerm;
+	
 	class CAstAbstractVisitor {
 	public:
-		//virtual std::any visitClass(nodePtr& node) = 0;
+		virtual void visit(Class* classNode) = 0;
+		virtual void visit(Method* method) = 0;
+		virtual void visit(Block* block) = 0;
+		virtual void visit(NestedBlock* nestedBlock) = 0;
+		virtual void visit(UnaryPattern* unaryPattern) = 0;
+		virtual void visit(BinaryPattern* binaryPattern) = 0;
+		virtual void visit(KeywordPattern* keywordPattern) = 0;
+		virtual void visit(Keyword* keyword) = 0;
+
+		virtual void visit(UnarySelector* unarySelector) = 0;
+		virtual void visit(BinarySelector* binarySelector) = 0;
+		virtual void visit(KeywordSelector* keywordSelector) = 0;
+
+		virtual void visit(UnaryMessage* unaryMessage) = 0;
+		virtual void visit(BinaryMessage* binaryMessage) = 0;
+		virtual void visit(BinaryOperand* binaryOperand) = 0;
+		virtual void visit(KeywordMessage* keywordMessage) = 0;
+		
+		virtual void visit(Formula* formula) = 0;
+
+		virtual void visit(LiteralInteger* litInteger) = 0;
+		virtual void visit(LiteralString* litString) = 0;
+		virtual void visit(LiteralArray* litArray) = 0;
+		virtual void visit(LiteralDouble* litDouble) = 0;
+		
+		virtual void visit(Assignation* assignation) = 0;
+		virtual void visit(Evaluation* evaluation) = 0;
+		virtual void visit(Variable* variable) = 0;
+		virtual void visit(NestedTerm* nestedTerm) = 0;
 	};
 
 }
--- src/ast/ASTNodes.h
+++ src/ast/ASTNodes.h
@@ -3,6 +3,9 @@
 #include <memory>
 #include <vector>
 #include "antlr4-runtime.h"
+#include "ASTAbstractVisitor.h"
+
+#define VISITABLE virtual void accept(CAstAbstractVisitor& visitor) final { visitor.visit(this); }
 
 namespace som {
 
@@ -29,6 +32,7 @@ namespace som {
 		nodeVector m_instanceMethods;
 		nodeVector m_classFields;
 		nodeVector m_classMethods;
+		VISITABLE
 	};
 
 	// Methods and message patterns
@@ -39,52 +43,61 @@ namespace som {
 
 		nodePtr m_pattern;
 		nodePtr m_methodBlock;
+		VISITABLE
 	};
 
 	struct Block : ASTNode {
 		Block(nodeVector& localDefs, nodeVector& expressions) : m_localDefs(std::move(localDefs)), m_expressions(std::move(localDefs)) {}
 		nodeVector m_localDefs;
 		nodeVector m_expressions;
+		VISITABLE
 	};
 	struct NestedBlock : ASTNode {
 		NestedBlock(nodeVector& args, nodePtr block) : m_arguments(std::move(args)), m_block(std::move(block)) {}
 		nodeVector m_arguments;
 		nodePtr m_block;
+		VISITABLE
 	};
 
 
 	struct UnaryPattern : ASTNode {
 		UnaryPattern(std::string identifier) : m_identifier(std::move(identifier)) {}
 		std::string m_identifier;
+		VISITABLE;
 	};
 
 	struct BinaryPattern : ASTNode {
 		BinaryPattern(nodePtr binOp, nodePtr argument) : m_binOp(std::move(binOp)), m_argument(std::move(argument)) {}
 		nodePtr m_binOp;
 		nodePtr m_argument;
+		VISITABLE
 	};
 
 	struct KeywordPattern : ASTNode {
 		KeywordPattern(nodeVector& keywords, nodeVector& args) : m_keywords(std::move(keywords)), m_arguments(std::move(args)) {}
 		nodeVector m_keywords;
 		nodeVector m_arguments;
+		VISITABLE
 	};
 
 	struct Keyword : ASTNode {
 		Keyword(std::string keyword) : m_keyword(std::move(keyword)) {}
 		std::string m_keyword;
+		VISITABLE
 	};
 
 	struct KeywordWithArgs : ASTNode {
 		KeywordWithArgs(std::string identifier, nodePtr argument) : m_identifier(std::move(identifier)), m_argument(std::move(argument)) {}
 		std::string m_identifier;
 		nodePtr m_argument;
+		VISITABLE
 	};
 
 	// Method selectors
 	struct UnarySelector : ASTNode {
 		UnarySelector(std::string identifier) : m_identifier(identifier) {}
 		std::string m_identifier;
+		VISITABLE
 	};
 
 	enum class BinaryOperation {
@@ -109,39 +122,46 @@ namespace som {
 	struct BinarySelector : ASTNode {
 		BinarySelector(BinaryOperation type) : m_type(type) {}
 		BinaryOperation m_type;
+		VISITABLE
 	};
 
 	struct KeywordSelector : ASTNode {
 		KeywordSelector(nodeVector& keywords) : m_keywords(std::move(keywords)) {}
 		nodeVector m_keywords;
+		VISITABLE
 	};
 
 	// Message sends
 	struct UnaryMessage : ASTNode {
 		UnaryMessage(nodePtr selector) : m_unarySelector(std::move(selector)) {}
 		nodePtr m_unarySelector;
+		VISITABLE
 	};
 
 	struct BinaryMessage : ASTNode {
 		BinaryMessage(nodePtr selector, nodePtr operand) : m_binarySelector(std::move(selector)), m_operand(std::move(operand)) {}
 		nodePtr m_binarySelector;
 		nodePtr m_operand;
+		VISITABLE
 	};
 	struct BinaryOperand : ASTNode {
 		BinaryOperand(nodePtr primary, nodeVector& messages) : m_primary(std::move(primary)), m_unaryMessages(std::move(messages)) {}
 		nodePtr m_primary;
 		nodeVector m_unaryMessages;
+		VISITABLE
 	};
 
 	struct KeywordMessage : ASTNode {
 		KeywordMessage(nodeVector& keywords, nodeVector& formulas) : m_keywords(std::move(keywords)), m_formulas(std::move(formulas)) {}
 		nodeVector m_keywords;
 		nodeVector m_formulas;
+		VISITABLE
 	};
 	struct Formula : ASTNode {
 		Formula(nodePtr arg, nodeVector& binaryMessage) : m_argument(std::move(arg)), m_binaryMessage(std::move(binaryMessage)) {}
 		nodePtr m_argument;
 		nodeVector m_binaryMessage;
+		VISITABLE
 	};
 
 
@@ -149,43 +169,51 @@ namespace som {
 	struct LiteralInteger : ASTNode {
 		LiteralInteger(int32_t value) : m_value(value) {}
 		int32_t m_value;
+		VISITABLE
 	};
 
 	struct LiteralDouble : ASTNode {
 		LiteralDouble(double value) : m_value(value) {}
 		double m_value;
+		VISITABLE
 	};
 
 	struct LiteralString : ASTNode {
 		LiteralString(std::string value) : m_value(std::move(value)) {}
 		std::string m_value;
+		VISITABLE
 	};
 
 	struct LiteralArray : ASTNode {
 		LiteralArray(nodeVector& literals) : m_literals(std::move(literals)) {}
 		nodeVector m_literals;
+		VISITABLE
 	};
 
 	struct Assignation : ASTNode {
 		Assignation(nodeVector& variables, nodePtr value) : m_variables(std::move(variables)), m_value(std::move(value)) {}
 		nodeVector m_variables;
 		nodePtr m_value;
+		VISITABLE
 	};
 
 	struct Evaluation : ASTNode {
 		Evaluation(nodePtr primary, nodeVector& messages) : m_primary(std::move(primary)), m_messages(std::move(messages)) {}
 		nodePtr m_primary;
 		nodeVector m_messages;
+		VISITABLE
 	};
 
 	struct Variable : ASTNode {
 		Variable(std::string identifier) : m_identifier(identifier) {}
 		std::string m_identifier;
+		VISITABLE
 	};
 
 	struct NestedTerm : ASTNode {
 		NestedTerm(nodePtr exp) : m_expression(std::move(exp)) {}
 		nodePtr m_expression;
+		VISITABLE
 	};
 
 }
